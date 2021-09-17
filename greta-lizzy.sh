5c4dd149c3e44b7eb38e6c9045efad2261f3ce74
Susan Allen <sallen@eoas.ubc.ca>
2021-09-17 10:18:35 -0700

    Edited and cleaned up the list of excluded directories for greta

--- greta-lizzy.sh
+++ greta-lizzy.sh
@@ -9,7 +9,7 @@ info "Preparing for for ${HOSTNAME} backup to lizzy"
 BORG=/usr/bin/borg
 
 # Avoid the need to give repo path/URL on the commandline:
-export BORG_REPO=lizzy:/media/susan/backup/borg/greta
+export BORG_REPO=lizzy:/media/doug/backup/borg/greta
 
 # Read repository passphrase from file:
 export BORG_PASSCOMMAND="cat ${HOME}/.borg-passphrase"
@@ -18,7 +18,6 @@ info "Starting ${HOSTNAME} backup to lizzy"
 
 # Backup the most important directories into an archive named after
 # the machine this script is currently running on:
-WAREHOUSE=/media/susan/warehouse
 
 ${BORG} create                                                   \
     --verbose                                                    \
@@ -39,17 +38,15 @@ ${BORG} create                                                   \
     --exclude "${HOME}/.vscode"                                  \
     --exclude "${HOME}/Downloads"                                \
     --exclude "${HOME}/snap"                                     \
-    --exclude "${WAREHOUSE}/.Trash-1000"                         \
-    --exclude "${WAREHOUSE}/conda_envs"                          \
-    --exclude "${WAREHOUSE}/Downloads"                           \
-    --exclude "${WAREHOUSE}/lost+found"                          \
-    --exclude "${WAREHOUSE}/miniconda3"                          \
-    --exclude "${WAREHOUSE}/snap"                                \
-                                                                 \
+    --exclude "${HOME}/conda-envs"                          \
+    --exclude "${HOME}/miniconda3"                          \
+    --exclude "${HOME}/.local/share/Trash"                   \
+    --exclude "${HOME}/.config/Slack/Cache"                \
+    --exclude "${HOME}/Desktop/Old Firefox Data"            \
+    --exclude "sh:${HOME}**/SalishSea_1h_*.nc"                   \
+                                                                  \
     ::'{hostname}-{now}'                                         \
     ${HOME}                                                      \
-    ${WAREHOUSE}                                                 \
-
 
 backup_exit=$?
 
